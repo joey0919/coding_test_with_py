495fcf421b8579e9109379b087d61b1503d4e4c3
천동희 <87600842+joey0919@users.noreply.github.com>
2023-11-05 22:27:15 +0900

    Add files via upload

---
+++ Sorting/03.퀵정렬_소스코드.py
@@ -0,0 +1,27 @@
+# 퀵정렬 예제코드
+# 시간복잡도 평균의 경우 O(Nlog(N) 최악의 경우 O(N^2)
+# 병합 정렬과 더불어 대부분의 프로그래밍 언어의 정렬 라이브러리의 근간이 되는 알고리즘
+# 가장 기본적인 퀵 정렬은 첫 번째 데이터를 기준 데이터(Pivot)로 설정한다.
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array, start, end):
+    if start >= end: # 원소가 1개인 경우 종료
+        return
+    pivot = start # 피벗은 첫 번째 요소
+    left = start + 1
+    right = end
+    while(left <= right):
+        # 피벗보다 큰 데이터를 찾을 때까지 반복
+        while(left <= end and array[left] <= array[pivot]):
+            left += 1
+        # 피벗보다 작은 데이터를 찾을 때까지 반복
+        while(right > start and array[right] >= array[pivot]):
+            right -= 1
+        if(left > right): # 엇갈렸다면 작은 데이터와 피벗을 교체
+            array[right], array[pivot] = array[pivot], array[right]
+        else: # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+            array[left], array[right] = array[right], array[left]
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+    quick_sort(array, start, right - 1)
+    quick_sort(array, right + 1, end)
+
